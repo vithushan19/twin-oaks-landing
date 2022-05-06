54aa983d1101440dc8a8e84d5601c7a37fbb8d73
winthyav <winthyav@gmail.com>
2022-05-06 09:43:18 +0100

    remove insta link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -40,8 +40,6 @@ export default function Landing() {
             />
           </div>
           <div className='mr-10'>
-          <a href='https://www.instagram.com/twinoaks.ie/'>
-          /a>
             <Image
               src='/images/Instagram.svg'
               alt='instagram'
