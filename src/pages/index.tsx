c6aec16bb4e5c92c4ad9a181ee3ff89c856f36cc
Vithushan Namasivayasivam <vithushan19@gmail.com>
2022-05-20 11:40:25 -0400

    feat: styling fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,34 +3,94 @@ import Link from 'next/link';
 
 export default function Landing() {
   return (
-    <div className='h-full grid grid-cols-1 bg-marble bg-repeat-round'>
-      <div className='hidden h-16 items-center justify-around bg-white bg-opacity-50 sm:flex'>
+    <div className='grid h-full grid-cols-1 bg-marble bg-repeat-round'>
+      <div className='flex justify-center p-4 sm:hidden'>
+        <div className='mr-10'>
+          <a
+            target='_blank'
+            href='https://twitter.com/twinoak02355553'
+            rel='noreferrer'
+          >
+            <Image
+              src='/images/twitter.svg'
+              alt='twitter'
+              width={20}
+              height={20}
+            />
+          </a>
+        </div>{' '}
+        <div className='mr-10'>
+          <a
+            target='_blank'
+            href='https://www.facebook.com/Twin-Oaks-Restaurant-114943181204346 '
+            rel='noreferrer'
+          >
+            <Image
+              src='/images/facebook.svg'
+              alt='facebook'
+              width={20}
+              height={20}
+            />
+          </a>
+        </div>
+        <div className=''>
+          <a
+            target='_blank'
+            href='https://www.instagram.com/twinoaks.ie/'
+            rel='noreferrer'
+          >
+            <Image
+              src='/images/Instagram.svg'
+              alt='instagram'
+              width={20}
+              height={20}
+            />
+          </a>
+        </div>
+      </div>
+      <div className='hidden h-16 items-center justify-between bg-white bg-opacity-50 sm:flex'>
+        {/* Phone */}
         <div className='flex items-center'>
-          <div className='mr-20 flex flex-col items-center'>
-            <Image src='/images/phone.svg' alt='phone' width={15} height={20} />
-          </div> {' '}
+          <div className='ml-8 flex items-center'>
+            <a href='tel:+442071234567'>
+              <div className='flex'>
+                <Image
+                  src='/images/phone.svg'
+                  alt='phone'
+                  width={15}
+                  height={20}
+                />
+                <p className='ml-4'>+442071234567</p>
+              </div>
+            </a>
+          </div>{' '}
           <p></p>
         </div>
+        {/* Nav Menu */}
         <div className='flex items-center'>
-        <div className='flex'>
-        <div className='mr-14'>
-            <Link href=''>MENU</Link>
-          </div>
-          <div className='mr-14'>
-            <Link href='#aboutus'>OUR STORY</Link>
-          </div>
-          <div className='mr-14'>
-            <Link href='#careers'>CAREERS</Link>
-          </div>
-          <div className='mr-14'>
-            <Link href='#hours'>CONTACT</Link>
+          <div className='flex'>
+            <div className='mr-14'>
+              <Link href=''>MENU</Link>
+            </div>
+            <div className='mr-14'>
+              <Link href='#aboutus'>OUR STORY</Link>
+            </div>
+            <div className='mr-14'>
+              <Link href='#careers'>CAREERS</Link>
+            </div>
+            <div className=''>
+              <Link href='#hours'>CONTACT</Link>
+            </div>
           </div>
         </div>
-        </div>
+        {/* Social Links */}
         <div className='flex'>
           <div className='mr-10'>
-        
-            <a target='_blank' href='https://twitter.com/twinoak02355553' rel='noreferrer'>
+            <a
+              target='_blank'
+              href='https://twitter.com/twinoak02355553'
+              rel='noreferrer'
+            >
               <Image
                 src='/images/twitter.svg'
                 alt='twitter'
@@ -40,33 +100,38 @@ export default function Landing() {
             </a>
           </div>{' '}
           <div className='mr-10'>
-          
-          <a target='_blank' href='https://www.facebook.com/Twin-Oaks-Restaurant-114943181204346 ' rel='noreferrer'>
-            <Image
-              src='/images/facebook.svg'
-              alt='facebook'
-              width={20}
-              height={20}
-            />
+            <a
+              target='_blank'
+              href='https://www.facebook.com/Twin-Oaks-Restaurant-114943181204346 '
+              rel='noreferrer'
+            >
+              <Image
+                src='/images/facebook.svg'
+                alt='facebook'
+                width={20}
+                height={20}
+              />
             </a>
           </div>
           <div className='mr-10'>
-       
-          <a target='_blank' href='https://www.instagram.com/twinoaks.ie/' rel='noreferrer'>
-            <Image
-              src='/images/Instagram.svg'
-              alt='instagram'
-              width={20}
-              height={20}
-            />
-             </a>
+            <a
+              target='_blank'
+              href='https://www.instagram.com/twinoaks.ie/'
+              rel='noreferrer'
+            >
+              <Image
+                src='/images/Instagram.svg'
+                alt='instagram'
+                width={20}
+                height={20}
+              />
+            </a>
           </div>
         </div>
       </div>
 
-      
-
-      <div className=' h-full flex w-full flex-col items-center justify-center p-16 sm:p-20 lg:p-40'>
+      <div className=' flex h-full w-full flex-col items-center justify-center p-16 sm:p-20 lg:p-40'>
+        {/* Desktop Only */}
         <div className='hidden sm:block'>
           {' '}
           <Image
@@ -76,7 +141,8 @@ export default function Landing() {
             height={174}
           />
         </div>
-        <div className='sm:hidden'>
+        {/* Mobile Only */}
+        <div className='mb-8 sm:hidden'>
           {' '}
           <Image
             src='/images/tree.svg'
@@ -85,13 +151,18 @@ export default function Landing() {
             height={158}
             objectFit='contain'
           />
-          <p className='font-playfair text-5xl w-full text-center mb-8 flex-col'>TWIN OAKS</p>
+          <p className='mb-8 w-full flex-col text-center font-playfair text-5xl'>
+            TWIN OAKS
+          </p>
         </div>
-        <button className='rounded-sm bg-brown px-6 py-4 text-white mb-40 font-lato font-light'>
+        <button className='font-lato mb-40 rounded-sm bg-brown px-6 py-4 font-light text-white'>
           Reserve a Table
         </button>
       </div>
-      <div id='aboutus' className='font-divlayfair flex w-full flex-col items-center bg-navyBlue px-16 text-center text-white sm:px-60'>
+      <div
+        id='aboutus'
+        className='font-divlayfair flex w-full flex-col items-center bg-navyBlue px-16 text-center text-white sm:px-60'
+      >
         <p className='mb-5 mt-20 font-playfair text-4xl'>Our Story</p>
         <div className='mb-16 h-px w-full bg-brown sm:w-110' />
         <p className='mb-8 font-roboto text-lg font-light'>
@@ -108,7 +179,10 @@ export default function Landing() {
         </div>
       </div>
 
-      <div id='careers'className='grid grid-cols-1 font-roboto text-white sm:grid-cols-2'>
+      <div
+        id='careers'
+        className='grid grid-cols-1 font-roboto text-white sm:grid-cols-2'
+      >
         <div className='bg-greyBlue pt-0 pl-16 pr-16 sm:pr-32 sm:pl-36'>
           <p className='mt-20 mb-5 font-playfair text-4xl'>Careers</p>
 
@@ -144,7 +218,10 @@ export default function Landing() {
         </div>
       </div>
 
-      <div id='hours'className='grid grid-cols-1 bg-marble bg-repeat-round font-roboto text-xl sm:grid-cols-2'>
+      <div
+        id='hours'
+        className='grid grid-cols-1 bg-marble bg-repeat-round font-roboto text-xl sm:grid-cols-2'
+      >
         <div className='px-16 sm:px-36'>
           <div className='mt-16'>
             <Image
@@ -153,8 +230,6 @@ export default function Landing() {
               width={268}
               height={158}
               objectFit='contain'
-          
-    
             />
             <p className='font-playfair text-5xl'>TWIN OAKS</p>
           </div>
@@ -162,56 +237,64 @@ export default function Landing() {
           <p className='mt-10 mb-8 font-playfair text-3xl'>Opening Hours</p>
 
           <div className='mt-5 flex flex-col'>
-            <p className='font-roboto text-xl font-regular text-amber-900'>Monday & Tuesday </p>
-            <p className='font-roboto text-xl font-light mt-2'>Closed </p>
+            <p className='font-regular font-roboto text-xl text-amber-900'>
+              Monday & Tuesday{' '}
+            </p>
+            <p className='mt-2 font-roboto text-xl font-light'>Closed </p>
           </div>
 
           <div className='mt-5 flex flex-col'>
-            <p className='font-roboto text-xl font-regular text-amber-900'>
+            <p className='font-regular font-roboto text-xl text-amber-900'>
               Wedenesday & Thursday{' '}
             </p>
-            <p className='font-roboto text-xl font-light mt-2'>18:30 - 21:30 </p>
+            <p className='mt-2 font-roboto text-xl font-light'>
+              18:30 - 21:30{' '}
+            </p>
           </div>
 
           <div className='mt-5 flex flex-col'>
-            <p className='font-roboto text-xl font-regular text-amber-900'>Friday - Sunday </p>
-            <p className='font-roboto text-xl font-light mt-2'>12:00 - 21:30 </p>
+            <p className='font-regular font-roboto text-xl text-amber-900'>
+              Friday - Sunday{' '}
+            </p>
+            <p className='mt-2 font-roboto text-xl font-light'>
+              12:00 - 21:30{' '}
+            </p>
           </div>
 
           <p className='mt-10 mb-8 font-playfair text-3xl'>Contact</p>
 
           <div className='mt-5 flex flex-col'>
             <div className='flex items-center'>
-              <div className='mr-0 flex flex-col items-center'>
-               
-              </div>
+              <div className='mr-0 flex flex-col items-center'></div>
               <p className='font-regular text-amber-900'>Email</p>
             </div>
-            <p className='font-roboto text-xl font-light mt-2'>
+            <p className='mt-2 font-roboto text-xl font-light'>
               info@twinoaksrestaurant{' '}
             </p>
           </div>
 
           <div className='mt-5 flex flex-col'>
             <div className='flex'>
-              <div className='mr-0'>
-                
-              </div>
+              <div className='mr-0'></div>
               <p className='font-regular text-amber-900'>Phone</p>
             </div>
 
-            <p className='font-roboto text-xl font-light mt-2'>+353 87 908 7089 </p>
+            <p className='mt-2 font-roboto text-xl font-light'>
+              +353 87 908 7089{' '}
+            </p>
           </div>
 
           <div className='mt-5 mb-20 flex flex-col'>
             <div className='flex'>
-              <div className='mr-0'>
-                
-              </div>
+              <div className='mr-0'></div>
               <p className='font-regular text-amber-900'>Address</p>
             </div>
-            <p className='font-roboto text-xl font-light mt-2'>Unit 10, Ashleigh Retail Center, </p>
-            <p className='font-roboto text-xl font-light'>Castleknock Village, D15 H9YD </p>
+            <p className='mt-2 font-roboto text-xl font-light'>
+              Unit 10, Ashleigh Retail Center,{' '}
+            </p>
+            <p className='font-roboto text-xl font-light'>
+              Castleknock Village, D15 H9YD{' '}
+            </p>
           </div>
         </div>
         <div className='mx-16 mb-16 mt-16 sm:mx-24 sm:mt-48'>
@@ -223,16 +306,16 @@ export default function Landing() {
       </div>
 
       <div className='grid grid-cols-12 items-center justify-center bg-navyBlue p-8 font-roboto font-light text-white'>
-        <div className='w-100 col-span-8 col-start-3 mb-16 mt-4 h-px bg-brown' />
-        <div className='col-span-4 col-start-5 flex flex-row justify-center'>
-          <p className='mr-10'>Careers</p>
-          <p className='mr-10'>Terms & Conditions</p>
+        <div className='w-100 col-span-8 col-start-3 mb-8 mt-4 h-px bg-brown' />
+        <div className='col-span-12 flex flex-col items-center justify-center sm:col-span-6 sm:col-start-4 sm:flex-row'>
+          <p className='sm:mr-10'>Careers</p>
+          <p className='my-8 sm:mr-10'>Terms & Conditions</p>
           <p className=''>Cookie Policy</p>
         </div>
-        <p className='col-span-4 col-start-5 my-8 text-center'>
+        <p className='col-span-12 my-8 text-center sm:col-span-4 sm:col-start-5'>
           © Twin Oaks 2022
         </p>
-        <p className='col-span-4 col-start-5 text-center'>
+        <p className='col-span-12 text-center sm:col-span-4 sm:col-start-5'>
           Designed & Developed by{' '}
           <Link href='https://skillify.ca' passHref={true}>
             <span className='text-yellow-600'>Skillify</span>
