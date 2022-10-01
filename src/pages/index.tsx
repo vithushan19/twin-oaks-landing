f8f8a2d7481d7008806b52211ccd0e24daf6576f
winthyav <winthyav@gmail.com>
2022-10-01 13:50:38 +0100

    text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -199,7 +199,7 @@ export default function Landing() {
         <p className='mb-5 mt-20 font-playfair text-4xl'>Gift Vouchers</p>
         <div className='mb-16 h-px w-full bg-brown sm:w-110' />
         <p className='mb-8 font-roboto text-lg font-light from-stone-900'>
-       Give the gift of dining at the Twin Oaks! E-gift cards are available to purchase at any value.  
+       Give the gift of dining at the Twin Oaks! E-gift vouchers are available to purchase at any value.  
         </p>
         <button className='font-lato mb-40 hidden rounded-sm bg-brown px-6 py-4 font-light text-white sm:block'>
           <Link href='https://gift.loylap.com/twinoaks/5898/amount=0erve'>Buy a Card</Link>
