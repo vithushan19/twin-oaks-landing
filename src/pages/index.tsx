483948eaf196278e14f8663bdf3bb3b77e267575
winthyav <winthyav@gmail.com>
2022-12-15 12:04:05 +0000

    Update index.tsx
    christmas hours update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -181,6 +181,27 @@ export default function Landing() {
       </div>
       <div
         id='giftcards'
+        className='font-divlayfair flex w-full flex-col items-center bg-greyBlue h px-16 text-center text-white sm:px-60'
+      >
+        <p className='mb-10 mt-10 font-playfair text-4xl'>Merry Christmas & Happy New Year from everyone at the Twin Oaks!</p>
+        
+        <p className='mb-8 font-roboto text-2xl font-light from-stone-900'>
+       Thank you for your support this year!  
+        </p>
+        <p className='mb-4 font-roboto text-xl font-Bold from-stone-900'>
+       Christmas Opening Hours  
+        </p>
+        <p className='mb-4 font-roboto text-lg font-light from-stone-900'>
+       The Twin Oaks will be closed from 5pm Saturday 24th of December to Tuesday 27th of December (Inclusive)
+        </p>
+        <p className='mb-8 font-roboto text-lg font-light from-stone-900'>
+       We will be re-opening at 5pm on Wednesday, December 28th. 
+        </p>
+        
+       
+      </div>
+      <div
+        id='giftcards'
         className='font-divlayfair flex w-full flex-col items-center bg-red-50 h px-16 text-center text-black sm:px-60'
       >
         <p className='mb-5 mt-20 font-playfair text-4xl'>Gift Vouchers</p>
