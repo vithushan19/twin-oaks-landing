332b5c0895bb0a170fcf716dd55ab722e19a1ed1
Vithushan Namasivayasivam <vithushan19@gmail.com>
2023-02-17 16:38:13 -0500

    create next app

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,4 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 const { fontFamily } = require('tailwindcss/defaultTheme');
-
-function withOpacityValue(variable) {
-  return ({ opacityValue }) => {
-    if (opacityValue === undefined) {
-      return `rgb(var(${variable}))`;
-    }
-    return `rgb(var(${variable}) / ${opacityValue})`;
-  };
-}
 
 /** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
 module.exports = {
